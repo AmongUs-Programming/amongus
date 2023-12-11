292f4f5d0680e399afc52f2e915aff8ca3eafe02
sycuuui <102959791+sycuuui@users.noreply.github.com>
2023-12-11 18:16:28 +0900

    feat : 방 생성 panel까지는 통신 완료

--- src/frame/ParticipantSetNameFrame.java
+++ src/frame/ParticipantSetNameFrame.java
@@ -34,30 +34,40 @@ public class ParticipantSetNameFrame extends JFrame {
                 new UserInfo(name);
                 System.out.println(name);
                 setVisible(false);
-                connectClient(name);
+                if(connectClient(name)){
+                    clientFrame.setPanelState(ClientFrame.PanelState.ROOM_LIST_PANEL);  // 상태를 ROOM_LIST_PANEL로 변경
+                }
             }
         });
         setVisible(true);
     }
-    private void connectClient(String name) {
-        SwingWorker<Void, Void> worker = new SwingWorker<Void, Void>() {
-            @Override
-            protected Void doInBackground() throws Exception {
-                client = new Client("127.0.0.1", 29998);
-                client.sendMessage("200/"+name);
-                clientFrame.setClient(client);  // ClientFrame에 Client 객체 저장
-                client.receiveMessage();
-                SwingWorker<Void, String> worker = new SwingWorker<Void, String>() {
-                    @Override
-                    protected Void doInBackground() throws Exception {
-                        clientFrame.setPanelState(ClientFrame.PanelState.ROOM_LIST_PANEL);  // 상태를 ROOM_LIST_PANEL로 변경
-                        return null;
-                    }
-                };
-                worker.execute();
-                return null;
-            }
-        };
-        worker.execute();
+    private Boolean connectClient(String name) {
+//        SwingWorker<Void, Void> worker = new SwingWorker<Void, Void>() {
+//            @Override
+//            protected Void doInBackground() throws Exception {
+//                client = new Client("127.0.0.1", 29998);
+//                client.sendMessage("200/"+name);
+//                clientFrame.setClient(client);  // ClientFrame에 Client 객체 저장
+//                client.receiveMessage();
+////                SwingWorker<Void, String> worker = new SwingWorker<Void, String>() {
+////                    @Override
+////                    protected Void doInBackground() throws Exception {
+////                        clientFrame.setPanelState(ClientFrame.PanelState.ROOM_LIST_PANEL);  // 상태를 ROOM_LIST_PANEL로 변경
+////                        return null;
+////                    }
+////                };
+////                worker.execute();
+////                return null;
+//                return null;
+//            }
+//        };
+//        worker.execute();
+        client = new Client("127.0.0.1", 29998);
+        client.sendMessage("200/"+name);
+        clientFrame.setClient(client);  // ClientFrame에 Client 객체 저장
+        client.receiveMessage();
+        if(client.receiveMessage().equals(null)){
+            return false;
+        }else return true;
     }
 }
