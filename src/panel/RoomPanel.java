a3c7b6502eb3fc51d8e22cfcba10c7ee618043c9
sycuuui <102959791+sycuuui@users.noreply.github.com>
2023-12-13 17:56:00 +0900

    fix : participantList까지 해결 완료

--- src/panel/RoomPanel.java
+++ src/panel/RoomPanel.java
@@ -20,57 +20,46 @@ public class RoomPanel extends JPanel {
 
     public RoomPanel(ClientFrame clientFrame) {
         this.clientFrame = clientFrame;
+        client = clientFrame.getClient();
         clientFrame.getClient().receiveMessage();
         message = clientFrame.getClient().getServerRealMessage();
-        client = clientFrame.getClient();
         System.out.println("message: " + message);
-        // RoomPanel에 leftPanel과 rightPanel을 추가합니다.
-        setLayout(new BorderLayout()); // GridLayout을 사용하여 왼쪽, 오른쪽 패널을 가로로 나란히 배치합니다.
-
-        // RoomParticipantListPanel과 RoomChatPanel을 생성
-        client.sendMessage("202/" + "userName");
-        client.receiveMessage();
-        userName = client.getServerRealMessage();
-
-        System.out.println("내 이름" + userName);
-        String roomTitle = clientFrame.getRoomTitle();
-        participantListPanel = new RoomParticipantListPanel(clientFrame, message, roomTitle);
-        // RoomParticipantListPanel과 RoomChatPanel을 생성
-        participantListPanel = new RoomParticipantListPanel(clientFrame, message, roomTitle);
-        RoomChatPanel roomchatPanel = new RoomChatPanel(userName);
-
         MessageThread messageThread = new MessageThread();
         messageThread.start();
+        setLayout(new BorderLayout()); // GridLayout을 사용하여 왼쪽, 오른쪽 패널을 가로로 나란히 배치합니다.
+
+        userName = client.getName();
+        String roomTitle = clientFrame.getRoomTitle();
+        participantListPanel = new RoomParticipantListPanel(clientFrame, message, roomTitle);
+        // 왼쪽에 배치될 panel
+        JPanel leftPanel = new JPanel(new BorderLayout());
+        add(leftPanel, BorderLayout.WEST); // RoomPanel의 서쪽에 leftPanel 추가
+        leftPanel.add(participantListPanel, BorderLayout.WEST); // participantListPanel을 왼쪽 패널에 추가
+        leftPanel.setPreferredSize(new Dimension(600, 600));
+        leftPanel.setPreferredSize(new Dimension(600, 600));
+
 
         JButton startBtn = new JButton("게임시작");
         startBtn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                messageThread.interrupt();
+//                messageThread.interrupt();
                 clientFrame.setPanelState(ClientFrame.PanelState.GAME_PANEL);
 
             }
         });
         add(startBtn, BorderLayout.NORTH);
 
-        // 왼쪽에 배치될 panel
-        JPanel leftPanel = new JPanel(new BorderLayout());
-        add(leftPanel, BorderLayout.WEST); // RoomPanel의 서쪽에 leftPanel 추가
-        leftPanel.add(participantListPanel, BorderLayout.WEST); // participantListPanel을 왼쪽 패널에 추가
-
 //
 //            // 오른쪽에 배치될 panel
-        JPanel rightPanel = new JPanel(new BorderLayout());
-        add(rightPanel, BorderLayout.CENTER);
-        roomchatPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
-        rightPanel.add(roomchatPanel, BorderLayout.CENTER); // chatPanel을 오른쪽 패널에 추가
-
-
-        leftPanel.setPreferredSize(new Dimension(600, 600));
+        //        RoomChatPanel roomchatPanel = new RoomChatPanel(userName);
+//        JPanel rightPanel = new JPanel(new BorderLayout());
+//        add(rightPanel, BorderLayout.CENTER);
+//        roomchatPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
+//        rightPanel.add(roomchatPanel, BorderLayout.CENTER); // chatPanel을 오른쪽 패널에 추가
+//
 //            rightPanel.setPreferredSize(new Dimension(500, 600));
-
-        leftPanel.setPreferredSize(new Dimension(600, 600));
-        rightPanel.setPreferredSize(new Dimension(500, 600));
+//        rightPanel.setPreferredSize(new Dimension(500, 600));
 
 
         setVisible(true);
@@ -80,6 +69,7 @@ public class RoomPanel extends JPanel {
     public class MessageThread extends Thread {
         @Override
         public void run() {
+//            clientFrame.getClient().sendMessage("401/"+clientFrame.getRoomTitle());
             clientFrame.getClient().receiveMessage();
             String message2 = clientFrame.getClient().getServerRealMessage();
             System.out.println("message2: " + message2);
